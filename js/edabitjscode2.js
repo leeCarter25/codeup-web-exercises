b25488b94be5d3fc77ae45caa9963d8bfe07aa4a
Christian Carter <carter.lee2028@gmail.com>
2022-08-09 21:58:11 -0500

    edabit exercises

---
+++ js/edabitjscode2.js
@@ -0,0 +1,36 @@
+
+    function triArea(base, height) {
+    return base * height / 2;
+}
+
+    function nextEdge(side1, side2) {
+        return side1 + side2 - 1;
+    }
+
+    function nameString(name){
+        var b = "test"
+        var result = name + b
+        return result;
+
+        function lessThanOrEqualToZero(num) {
+            if(num <= 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+    function getFirstValue(arr) {
+        return arr[0];
+    }
+
+    function addition(a, b) {
+        return a + b;
+    }
+    function findPerimeter(length, width) {
+        return (length * 2) + (width * 2);
+    }
+
+    function sumPolygon(n) {
+        return (n - 2) * 180;
+    }
