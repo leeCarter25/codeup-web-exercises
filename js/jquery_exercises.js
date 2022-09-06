f158814affc9446b25cd51ce19a99593c53ec131
Christian Carter <carter.lee2028@gmail.com>
2022-09-06 10:03:28 -0500

    jquery selectors exercise complete

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,3 +1,21 @@
 $(function() {
-    alert( 'The DOM has finished loading!' );
-});
+    alert( 'option2' );
+    document.getElementById('option2');
+
+    alert($("#option2").text());
+    alert($("#option3").text());
+    $('.codeup').css('border', '1px solid red');
+    $("li").css("font-size", "20px").css('background', 'yellow');
+    $("li").css("background", 'yellow');
+    $("p").css("background", 'yellow');
+    $("h1").css("background", 'yellow');
+
+
+    alert($('h1').html());
+
+
+
+
+
+
+})
